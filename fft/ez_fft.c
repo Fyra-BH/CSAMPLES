6081daaab571392e39ea409158dace621972a311
Fyra-BH <Fyra-BH@https://github.com>
2021-03-02 18:53:44 +0800

    add fft

--- fft/ez_fft.c
+++ fft/ez_fft.c
@@ -234,7 +234,7 @@ void fft_32(float* buff)
     for (size_t i = 0; i < 32; i++)
     {
         buff_c[i] = tmp[bit_reverse(i, 5)];
-        printf("buff_c[%d] = tmp[%d];\n", i, bit_reverse(i, 5));
+        // printf("buff_c[%d] = tmp[%d];\n", i, bit_reverse(i, 5));
     }
     complex_t w0 = W(32, 0);
     complex_t w1 = W(32, 1);
@@ -343,53 +343,4 @@ void fft_32(float* buff)
         *(buff + i * 2) = buff_c[i].re;
         *(buff + i * 2 + 1) = buff_c[i].im;
     }
-}
-
-int main(int argc, char const *argv[])
-{
-/*    
-    float a = 0.8;
-    int N = 8;
-    float x[16];
-    for (size_t i = 0; i < N; i++)
-    {
-        x[i * 2] =pow(a, (float)i);
-        x[i * 2 + 1] = 0;
-    }
-    fft_8(x);
-    for (size_t i = 0; i < 8; i++)
-    {
-        printf("%0.4f, j %0.4f\n", *(x + i * 2), *(x + i * 2 + 1));
-    }
-*/
-/*  
-    float a = 0.8;
-    int N = 16;
-    float x[32];
-    for (size_t i = 0; i < N; i++)
-    {
-        x[i * 2] =pow(a, (float)i);
-        x[i * 2 + 1] = 0;
-    }
-    fft_16(x);
-    for (size_t i = 0; i < 16; i++)
-    {
-        printf("%0.4f, j %0.4f\n", *(x + i * 2), *(x + i * 2 + 1));
-    }
-*/
-    float a = 0.8;
-    int N = 32;
-    float x[64];
-    for (size_t i = 0; i < N; i++)
-    {
-        x[i * 2] =pow(a, (float)i);
-        x[i * 2 + 1] = 0;
-    }
-    //测试32点fft
-    fft_32(x);
-    for (size_t i = 0; i < 32; i++)
-    {
-        printf("%0.4f, j %0.4f\n", *(x + i * 2), *(x + i * 2 + 1));
-    }
-    return 0;
 }
